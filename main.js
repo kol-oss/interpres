b504b408966fdf3539ebd743760abaa86a057599
kol-oss <kolyakhozhajinov@gmail.com>
2024-03-04 13:09:21 +0200

    refactor #4: refactored parser logic
    - removed closures;
    - took useful functions to utils;
    - created ultimate regular expressions;
    - updated config according to changes;
    - removed DEFAULT_REGEXPS object;
    - replaced input file to the root.
    
    closes #4

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 'use strict';
 
-const cli = require('./lib/interfaces/command');
+const parser = require('./lib/parser.js');
+const manager = require('./lib/interfaces/manager.js');
+const command = require('./lib/interfaces/command.js');
 
-function main() {
-  const { args, options } = cli;
+(async function main() {
+  try {
+    const { args, options } = command;
 
-  console.log(args, options);
-}
+    let result = '';
+    for (const src of args) {
+      const data = await manager.read(src);
+      result += parser.render(data);
+    }
 
-main();
+    const { out } = options;
+    if (out) await manager.write(out, result);
+    else console.log(result);
+  } catch (error) {
+    console.log('Error:', error.message);
+  }
+})();
